a0ead8672153f1654525456bcc39cbe99c9d4194
sulubin <sulubin123@163.com>
2016-08-18 13:05:11 +0800

    添加服务检查脚本

--- src/main/scala/com/changtu/api/ApiCheck.scala
+++ src/main/scala/com/changtu/api/ApiCheck.scala
@@ -14,6 +14,7 @@ import spray.can.Http
 import spray.client.pipelining._
 import spray.util._
 
+import scala.collection.mutable.ListBuffer
 import scala.concurrent.Future
 import scala.concurrent.duration._
 import scala.util.{Failure, Success}
@@ -33,23 +34,30 @@ object ApiCheck extends App with Logging {
 
   val pipeline = sendReceive ~> unmarshal[UserLabels]
   val Array(leaders, followers) = args
+  val errorApi = ListBuffer.empty[String]
 
-  leaders.split(",").foreach( p => {
+  leaders.split(",").foreach(p => {
     val host = p.split(":")(0)
     val responseFuture = pipeline {
       Get("http://".concat(host).concat(":9993/userlabel?userId=5236317&labelCode=coach_spot_class"))
     }
-    handleFuture(responseFuture)
+    handleFuture(responseFuture, host)
+    //errorApi += host
   })
 
+  if (errorApi.nonEmpty) {
+    logger.error("The following host get something wrong:".concat(errorApi.reduce( _ concat "," concat _)))
+    System.exit(1)
+  }
 
   /**
     * 重启leader
+    *
     * @param hosts 主机名，多台主机，用逗号分隔
     */
   def restartLeader(hosts: String): Unit = {
 
-    hosts.split(",").foreach( p => {
+    hosts.split(",").foreach(p => {
       val host = p.split(":")(0)
       val port = p.split(":")(1)
       // 密码解密
@@ -64,7 +72,7 @@ object ApiCheck extends App with Logging {
       val ssh = (cmd: String) => SSH(host, "hadoop", port.toInt, cmd, "", password)
       logger.info("restarting flower host : ".concat(host))
       ssh("ps -ef | grep com.changtu.api.ApiBoot | grep 9993 | grep -v grep | awk '{print $2}' | xargs kill")
-      //Thread.sleep(2000)
+      Thread.sleep(1000)
       ssh("nohup java -Djava.ext.dirs=/appl/scripts/e-business/rest/target/lib -classpath /appl/scripts/e-business/rest/target/rest-1.0-SNAPSHOT.jar com.changtu.api.ApiBoot ".concat(host).concat(" 9993 > /appl/scripts/e-business/rest/out.follower.$$.log 2>&1 &"))
       //ssh("nohup")
     })
@@ -73,11 +81,12 @@ object ApiCheck extends App with Logging {
 
   /**
     * 重启follower
+    *
     * @param hosts 主机名，多台主机，用逗号分隔
     */
   def restartFlower(hosts: String): Unit = {
 
-    hosts.split(",").foreach( p => {
+    hosts.split(",").foreach(p => {
       val host = p.split(":")(0)
       val port = p.split(":")(1)
       // 密码解密
@@ -101,12 +110,14 @@ object ApiCheck extends App with Logging {
 
   /**
     * 处理异常接口
+    *
     * @param responseFuture 响应线程
     */
-  def handleFuture(responseFuture: Future[UserLabels]): Unit = {
+  def handleFuture(responseFuture: Future[UserLabels], host: String): Unit = {
     responseFuture onComplete {
       case Success(labels: UserLabels) =>
-        logger.info("The API call was successful and get the right labels".concat(labels.asInstanceOf[UserLabels].labels))
+        logger.info("The API call was successful and get the right labels:".concat(labels.asInstanceOf[UserLabels].labels))
+        errorApi += host
         shutdown()
       case Success(somethingUnexpected) =>
         logger.warn("The API call was successful but returned something unexpected: '{}'.".concat(somethingUnexpected.toString))
